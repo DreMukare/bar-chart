13638bb1130c562ffa5a1af96a412d28f049f500
JudgeFudge19 <dremukare@gmail.com>
2020-09-07 00:59:24 +0300

    Created svg area. Rect not appearing yet

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const url =
-	"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+document.addEventListener("DOMContentLoaded", () => {
+	const url =
+		"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-fetch(url)
-	.then((res) => res.json())
-	.then((data) => {
-		document.getElementById("svg").innerHTML = `<p>${JSON.stringify(data.data)}
-			</p>`;
-	});
+	let dataset;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			dataset = JSON.parse(JSON.stringify(data.data));
+
+			const w = 1200;
+			const h = 500;
+			const padding = 30;
+
+			const xScale = d3
+				.scaleLinear()
+				.domain([0, d3.max(dataset, (d) => d[0].slice(0, 4))])
+				.range([[padding, w - padding]]);
+			const yScale = d3
+				.scaleLinear()
+				.domain([0, d3.max(dataset, (d) => d[1])])
+				.range([h - padding, padding]);
+
+			console.log(dataset);
+
+			const svg = d3.select("#svg");
+
+			svg.append("svg").attr("width", w).attr("height", h);
+
+			svg
+				.selectAll("rect")
+				.data(dataset)
+				.enter()
+				.append("rect")
+				.attr("fill", "black")
+				.attr("x", (d, i) => i * 30)
+				.attr("y", (d) => yScale(h - d[1]));
+		});
+});
