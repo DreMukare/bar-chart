123fa4ccb5594d55681a5462ccacebc9a9a60971
JudgeFudge19 <dremukare@gmail.com>
2020-09-08 02:10:24 +0300

    Passed a few more tests. Need to add custom tooltip and align bars with axes

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
+// Run everything only after the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+	// Url to be used in fetch request
 	const url =
 		"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-	let dataset;
+	// fetching data from api
 	fetch(url)
 		.then((res) => res.json())
 		.then((data) => {
-			dataset = JSON.parse(JSON.stringify(data.data));
+			const dataset = JSON.parse(JSON.stringify(data.data));
 
-			const w = 1200;
-			const h = 500;
-			const padding = 30;
-
-			const xScale = d3
-				.scaleLinear()
-				.domain([0, d3.max(dataset, (d) => d[0].slice(0, 4))])
-				.range([[padding, w - padding]]);
-			const yScale = d3
-				.scaleLinear()
-				.domain([0, d3.max(dataset, (d) => d[1])])
-				.range([h - padding, padding]);
-
-			console.log(dataset);
-
-			const svg = d3.select("#svg");
-
-			svg.append("svg").attr("width", w).attr("height", h);
-
-			svg
-				.selectAll("rect")
-				.data(dataset)
-				.enter()
-				.append("rect")
-				.attr("fill", "black")
-				.attr("x", (d, i) => i * 30)
-				.attr("y", (d) => yScale(h - d[1]));
+			// using the data in the function to create the bar chart
+			createBarChart(dataset);
 		});
 });
+
+const createBarChart = (data) => {
+	// setting sizes of different elements and spacing between them
+	const w = 1200;
+	const h = 500;
+	const padding = 50;
+	const widthOfBar = (w - 2 * padding) / data.length;
+
+	// creating <svg> element
+	const svg = d3
+		.select("#svg")
+		.append("svg")
+		.attr("height", h)
+		.attr("width", w);
+
+	// creating scale to use in height of bars and x and y axes
+	const xScale = d3
+		.scaleLinear()
+		.domain([
+			d3.min(data, (d) => d[0].split("-")[0]),
+			d3.max(data, (d) => d[0].split("-")[0]),
+		])
+		.range([padding, w - padding]);
+	const yScale = d3
+		.scaleLinear()
+		.domain([0, d3.max(data, (d) => d[1])])
+		.range([h - padding, padding]);
+
+	// creating <rect> element for every data point in dataset
+	svg
+		.selectAll("rect")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+		.attr("data-date", (d) => d[0])
+		.attr("data-gdp", (d) => d[1])
+		.attr("width", widthOfBar)
+		.attr("height", (d) => h - yScale(d[1]))
+		.attr("x", (d, i) => i * widthOfBar + padding)
+		.attr("y", (d) => yScale(d[1]) - padding);
+	// .on("mouseover", (d, i) => {
+	// 	const tooltip = document.getElementById("tooltip");
+
+	// 	tooltip.setAttribute("id", "tooltip");
+	// 	tooltip.setAttribute("x", i * widthOfBar + padding);
+	// 	tooltip.setAttribute("y", yScale(d[1]) - padding);
+	// 	tooltip.textContent(`${d[0]} \n $${d[1]} Billion`);
+	// });
+	// .append("title")
+	// .attr("id", "tooltip")
+	// .text((d) => `${d[0]} \n $${d[1]} Billion`);
+
+	// // creating <rect> for tooltip
+	// svg.append("rect").attr("id", "tooltip").attr("height", 30).attr("width", 70);
+
+	// NEED TO CREATE CUSTOM TOOLTIP
+
+	// creating axes
+	const xAxis = d3.axisBottom(xScale);
+	const yAxis = d3.axisLeft(yScale);
+
+	// visualizing x axis
+	svg
+		.append("g")
+		.attr("transform", `translate(0, ${h - padding})`)
+		.attr("id", "x-axis")
+		.call(xAxis);
+
+	// visualizing y axis
+	svg
+		.append("g")
+		.attr("transform", `translate(${padding}, 0)`)
+		.attr("id", "y-axis")
+		.call(yAxis);
+};
